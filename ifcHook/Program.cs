24e962d39ced3afe16f31cc2ff426f96254b0be5
Claudio Benghi <claudio.benghi@gmail.com>
2023-06-30 18:03:58 +0200

    dotnet executable hooked, always cancels commit

--- ifcHook/Program.cs
+++ ifcHook/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Preprocessing IFCs.");
 Console.WriteLine($"- running in {Environment.CurrentDirectory}");
 var list = ifcHook.GitInteraction.CallGit.Run("diff --name-only --cached");
 Console.WriteLine(list);
+Console.WriteLine("Cancelling commit");
 return -1;
